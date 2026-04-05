e9d2667ab38543f68c57dde30770bdb1ab36b916
create@darketomaly.hk <darketomaly@gmail.com>
2026-04-05 13:27:58 -0700

    Added PlatformProjectConfigData and updated core to 0.0.5

---
+++ Assets/com.darketomaly.framework.platforms/Scripts/Runtime/PlatformProjectConfigData.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Framework
+{
+    public class PlatformProjectConfigData : FrameworkModuleData
+    {
+        [field: SerializeField] 
+        public string PlayfabId { get; private set; }
+        
+        [MenuItem("Tools/Framework/Inject platform project config")]
+        private static void InjectIfNeeded()
+        {
+            var config = FrameworkProjectConfig.Instance;
+            if (config == null) return;
+
+            if (config.GetModule<PlatformProjectConfigData>() == null)
+            {
+                var playfabData = new PlatformProjectConfigData();
+                config.AddModule<PlatformProjectConfigData>(playfabData);
+
+                EditorUtility.SetDirty(config);
+                AssetDatabase.SaveAssets();
+
+                Debug.Log("[Framework] Added platform data to project config");
+            }
+        }
+    }
+}
